7077d73fd9af52557e4ad79d680bbee224ae1e75
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-16 17:59:58 -0600

    added feature: user's total portfolio value is now added and show at the bottom of the portfolio list

--- public/javascripts/controllers/portfolioCtrl.js
+++ public/javascripts/controllers/portfolioCtrl.js
@@ -6,10 +6,21 @@ app.controller("portfolioCtrl", ["$http", function($http)
 	//get data from mongo for each stock and output the various components, executes immediately
 	self.stocks = (() =>
 	{
+
+		//get stock data
 		$http.get("/api/portfolio")
 		.then((data)=>
 		{
 			self.stocksArray = data.data;
+
+			//initialize stock value
+			self.totalPortfolioValue=0;
+
+			//total each (stock * qty) and add result to totalPortfolioValue
+			data.data.map((item, index)=>{
+				self.totalPortfolioValue += (item.purchaseStockPrice * item.quantity);
+			})
+
 		})
 	})();
 
