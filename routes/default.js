b7a5e9d2b83a96d3f585b8dfeb30ed61af11269d
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-15 19:01:22 -0600

    added api integration

--- routes/default.js
+++ routes/default.js
@@ -1,10 +1,20 @@
 //default route
 const express = require('express');
 const router = express.Router();
+const test = require("../ctrlrs/test");
 
 router.get("/", (req, res) =>
 {
 	res.render("index")
+})
+
+
+router.get("/example", (req, res) =>
+{
+	let response = test.getNetflix(req, res)
+
+	//right now this will not work because this is async, so handle with front end code
+	res.send(response)
 })
 
 module.exports = router;
