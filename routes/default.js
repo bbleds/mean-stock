534efcbb15df9cef4856adb9a30c6a0f633699ec
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-15 19:10:22 -0600

    integrated bootstrap with jade

--- routes/default.js
+++ routes/default.js
@@ -1,3 +1,4 @@
+'use strict';
 //default route
 const express = require('express');
 const router = express.Router();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 //Get dependencies
 const express = require("express");
 const routes = require('./routes/');
+const path = require("path")
 //Initialize app
 const app = express();
 //set port to environment port or 3000
@@ -10,6 +11,9 @@ const PORT =process.env.PORT || 3000;
 
 //use jade templating engine
 app.set("view engine", "jade");
+
+//use public directory for static files
+app.use(express.static(path.join(__dirname, '/public')))
 
 //load all routes
 app.use(routes);
