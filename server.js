eba975216d8fe92edd5074d2ede4389dfcbf829b
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-16 18:12:57 -0600

    added sass middleware for sass compiling

--- server.js
+++ server.js
@@ -4,15 +4,28 @@
 const express = require("express");
 const routes = require('./routes/');
 const path = require("path")
+const mongoose = require("mongoose");
+const sassMiddleware = require('node-sass-middleware');
 //Initialize app
 const app = express();
 //set port to environment port or 3000
 const PORT =process.env.PORT || 3000;
-const mongoose = require("mongoose");
+//set url to mongodb
 const MONGODB_URL = "mongodb://localhost:27017/testStocks";
 
 //use jade templating engine
 app.set("view engine", "jade");
+
+//use sass compiler via express
+app.use(sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    //this is whitespace indent rather than curly braces
+    indentedSyntax: true,
+    debug: true,
+    outputStyle: 'compressed'
+}));
 
 //use public directory for static files
 app.use(express.static(path.join(__dirname, '/public')))
